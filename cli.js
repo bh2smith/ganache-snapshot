9c47c0350e305c8caed0379365937723192b68ef
Ben Smith <bh2smith@gmail.com>
2019-07-04 15:48:16 +0200

    use truffle plugin format

--- cli.js
+++ cli.js
@@ -1,46 +1,25 @@
-const { makeSnapshot, revertSnapshot } = require('./index.js')
+const Web3 = require("web3")
+const { makeSnapshot, revertSnapshot } = require("./index.js")
 
-module.exports = async (callback) => {
-  try {
-    const arguments = process.argv.slice(4)
-    if (arguments.length > 2) {
-      callback("Error: This script requires arguments - <make> or <revert> <snapshotId>")
-    }
-    const [todo, snapId] = arguments
 
-    if (todo == "make") {
-      console.log("Making snapshot")
-      const snapID = (await makeSnapshot(web3)).result
-      callback(snapID)
-    } else if (todo == "revert") {
-      console.log(`Reverting snapshot ${snapId}`)
-      await revertSnapshot(snapId, web3)
-      callback()
-    }
-  } catch(error) {
-    callback(error)
+/**
+ * Outputs snapshot id as a hex string when running `truffle run snapshot make`,
+ * or reverts snapshot by specified id when running `truffle run snapshot revert <snappID>`
+ * @param {Config} config - A truffle-config object.
+ */
+module.exports = async (config) => {
+  config.network = "development"  // This is a hack
+  var web3 = new Web3(config.provider)
+
+  // TODO - fix parsing
+  if (config._.length > 3) {
+    console.error("Error: This script requires arguments - <make> or <revert> <snapshotId>")
+  }
+  const [, todo, snapId] = config._
+  if (todo == "make") {
+    const snapID = (await makeSnapshot(web3)).result
+    console.log(snapID)
+  } else if (todo == "revert") {
+    await revertSnapshot(snapId, web3)
   }
 }
-
-// const run = async (callback) => {
-//     try {
-//     //   const arguments = process.argv.slice(4)
-//     const arguments = process.argv.slice(2)
-//       if (arguments.length > 2) {
-//         callback("Error: This script requires arguments - <make> or <revert> <snapshotId>")
-//       }
-//       const [todo, snapId] = arguments
-  
-//       if (todo == "make") {
-//         console.log("Making snapshot")
-//         const snapID = (await makeSnapshot(web3)).result
-//         callback(snapID)
-//       } else if (todo == "revert") {
-//         console.log(`Reverting snapshot ${snapId}`)
-//         await revertSnapshot(snapId, web3)
-//         callback()
-//       }
-//     } catch(error) {
-//       callback(error)
-//     }
-//   }
