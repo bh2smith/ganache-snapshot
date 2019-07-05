c75864eb1c3dcecf67df84597e8d8c03550edeea
Felix Leupold <felixleupold90@gmail.com>
2019-07-05 18:10:11 +0200

    Creating an e2e test for cli (#4)
    So far we have been testing the js code in a truffle unit test. This PR makes it that we are also testing the CLI tool in a shell script e2e test.

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
-var Migrations = artifacts.require("Migrations.sol")
+const Migrations = artifacts.require("Migrations.sol")
+const Example = artifacts.require("ExampleContract")
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations)
+  deployer.deploy(Example)
 }
