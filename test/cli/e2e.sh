aa34bfe63db0c92c819f19bff642686b70743d8a
Felix Leupold <felixleupold90@gmail.com>
2019-07-05 17:25:04 +0200

    not tailing output in e2e test

--- test/cli/e2e.sh
+++ test/cli/e2e.sh
@@ -2,22 +2,22 @@
 
 set -e
 
-STATE_BEFORE_SNAPSHOT=$(truffle exec test/cli/read_contract.js | tail -1)
+STATE_BEFORE_SNAPSHOT=$(truffle exec test/cli/read_contract.js)
 SNAP_ID=$(truffle run snapshot make)
 echo $STATE_BEFORE_SNAPSHOT
 
-STATE_AFTER_WRITE=$(truffle exec test/cli/write_contract.js | tail -1)
+STATE_AFTER_WRITE=$(truffle exec test/cli/write_contract.js)
 
-if [ "$STATE_AFTER_WRITE" -eq "$STATE_BEFORE_SNAPSHOT" ]
+if [ "$STATE_AFTER_WRITE" == "$STATE_BEFORE_SNAPSHOT" ]
 then
     echo "Expected write operation to change something"
     exit 1
 fi
 
 truffle run snapshot revert $SNAP_ID
-STATE_AFTER_REVERT=$(truffle exec test/cli/read_contract.js | tail -1)
+STATE_AFTER_REVERT=$(truffle exec test/cli/read_contract.js)
 
-if [ "$STATE_AFTER_REVERT" -ne "$STATE_BEFORE_SNAPSHOT" ]
+if [ "$STATE_AFTER_REVERT" != "$STATE_BEFORE_SNAPSHOT" ]
 then
     echo "Expected to revert state to before snapshot"
     exit 1
