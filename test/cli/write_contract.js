ec7e93dfd98f0ebfee10f37acf837c6954193655
Felix Leupold <felixleupold90@gmail.com>
2019-07-05 17:02:58 +0200

    Creating an e2e test for cli

---
+++ test/cli/write_contract.js
@@ -0,0 +1,12 @@
+const Example = artifacts.require("ExampleContract")
+
+module.exports = async (callback) => {
+    try {
+        const example = await Example.deployed()
+        await example.incrementValue()
+        console.log((await example.value.call()).toString())
+        callback()
+    } catch(err) {
+        callback(err)
+    }
+}
