aeadcce40b70c3ec2c9226696cf33702123b9b08
Felix Leupold <felixleupold90@gmail.com>
2019-07-05 17:40:48 +0200

    fetching single line again

--- test/cli/e2e.sh
+++ test/cli/e2e.sh
@@ -2,10 +2,23 @@
 
 set -e
 
-STATE_BEFORE_SNAPSHOT=$(truffle exec test/cli/read_contract.js)
+STATE_BEFORE_SNAPSHOT=$(truffle exec test/cli/read_contract.js | awk 'NR==3')
 SNAP_ID=$(truffle run snapshot make)
+echo $STATE_BEFORE_SNAPSHOT
 
-STATE_AFTER_WRITE=$(truffle exec test/cli/write_contract.js)
+STATE_AFTER_WRITE=$(truffle exec test/cli/write_contract.js | awk 'NR==3')
+
+if [ "$STATE_AFTER_WRITE" -eq "$STATE_BEFORE_SNAPSHOT" ]
+then
+    echo "Expected write operation to change something"
+    exit 1
+fi
 
 truffle run snapshot revert $SNAP_ID
-STATE_AFTER_REVERT=$(truffle exec test/cli/read_contract.js)
+STATE_AFTER_REVERT=$(truffle exec test/cli/read_contract.js | awk 'NR==3')
+
+if [ "$STATE_AFTER_REVERT" -ne "$STATE_BEFORE_SNAPSHOT" ]
+then
+    echo "Expected to revert state to before snapshot"
+    exit 1
+fi
