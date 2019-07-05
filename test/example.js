bd198b12fe928b8d2c0610fc548199d2dc6666c4
Ben Smith <bh2smith@gmail.com>
2019-07-05 16:19:11 +0200

    typo

--- test/example.js
+++ test/example.js
@@ -6,7 +6,7 @@ contract("ExampleContract", async () => {
   describe("incrementValue()", () => {
     it("makes snapshot, increments value and reverts snapshot", async () => {
       const example = await Example.new()                      // Deploy new contract instance
-      const snapID = await makeSnapshot(web3)         // Take a snapshot and save the returned ID
+      const snapID = await makeSnapshot(web3)                  // Take a snapshot and save the returned ID
       await example.incrementValue()                           // increment value "complicated sequence of transactions"
       assert.equal(1, (await example.value.call()).toNumber()) // check that value was incremented
       await revertSnapshot(snapID, web3)                       // revert snapshot (by ID)
