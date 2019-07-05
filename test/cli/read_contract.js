1b6be0bb285be7e5c7e3fee6da1762c95ae8d359
Felix Leupold <felixleupold90@gmail.com>
2019-07-05 17:15:27 +0200

    eslint

--- test/cli/read_contract.js
+++ test/cli/read_contract.js
@@ -1,11 +1,11 @@
 const Example = artifacts.require("ExampleContract")
 
 module.exports = async (callback) => {
-    try {
-        const example = await Example.deployed()
-        console.log((await example.value.call()).toString())
-        callback()
-    } catch(err) {
-        callback(err)
-    }
+  try {
+    const example = await Example.deployed()
+    console.log((await example.value.call()).toString())
+    callback()
+  } catch(err) {
+    callback(err)
+  }
 }
